b1f45990ac862fff65210d4efacad7cbabc61736
Samyr <saamyr@live.com>
2020-03-04 20:31:19 -0300

    Segunda aula do curso introdutório de JS, aprendido a declarar váriaves que são definidas automaticamente pelo sistema ao identificar o tipo de valor que possui, tambem a como escrever arrays ou vetores com [], usar o console.log a tirar e adicionar itens da arrya com .pop e .push, a subir alerta na tela do usuário com o alert(), a declarar tambem dicionários transformando o código similar a .json, tambem foi aprendido a medir o tamanho da array com o .length e a escreve-lá ao contrário com o .reverse, tambem a transformar o array para string com o .toString e a usar o join para separar transformando em string a array, e concatenar com o +.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/*
+var nome = "Samyr Ribeiro"
+var idade = 24;
+var idade2 = 5;
+var frase = "Jamaica é topzera bixão";
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2)
+console.log(nome)
+console.log(idade/idade2)
+console.log(frase.toUpperCase())
+frutas.replace("Jamaica","Holanda")
+*/
+//var frutas = ["maça","uva","banana"];
+//frutas.push("pedra");
+//frutas.pop();
+//console.log(frutas[3]);
+//alert(frutas[1]);
+//console.log(frutas.length);
+//console.log(frutas.reverse);
+//console.log(frutas.toString())
+//console.log(frutas.join(" | "))
+/*
+var frutas = {nome: "perera", cor: "preta", tamanho:"3 pés"}
+console.log(frutas);
+alert(frutas.cor);*/
+var frutas = [{nome: "perera", cor: "preta", tamanho:"3 pés"},{nome: "goiabra", cor: "veeerde", tamanho:"3 pés"}]
+console.log(frutas);
+alert(frutas[1].cor)
