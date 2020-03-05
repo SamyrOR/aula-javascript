f2c6eb9b2f03fd6424141a8219fee05ce173f990
Samyr <saamyr@live.com>
2020-03-04 21:17:23 -0300

    Terceira aula de JS, aprendido a declarar if e else, while, for, e a receber dados do usuário com prompt, retornar data e hora.

--- js/main.js
+++ js/main.js
@@ -23,6 +23,28 @@ frutas.replace("Jamaica","Holanda")
 var frutas = {nome: "perera", cor: "preta", tamanho:"3 pés"}
 console.log(frutas);
 alert(frutas.cor);*/
-var frutas = [{nome: "perera", cor: "preta", tamanho:"3 pés"},{nome: "goiabra", cor: "veeerde", tamanho:"3 pés"}]
-console.log(frutas);
-alert(frutas[1].cor)
+//var frutas = [{nome: "perera", cor: "preta", tamanho:"3 pés"},{nome: "goiabra", cor: "veeerde", tamanho:"3 pés"}]
+//console.log(frutas);
+//alert(frutas[1].cor)
+/*
+var idade = prompt("Qual é a sua idade meu jovem?: ")
+if (idade >= 18){
+    alert("Você é maior de idade")
+}else{
+    alert("Menor de idade")
+}
+*/
+/*
+var contadora = 5;
+while (contadora >=0){
+    console.log(contadora);
+    contadora--;
+}
+*/
+/*for(count=0;count<=5;count++){
+    alert(count)
+}
+*/
+var d = new Date;
+alert(d.getDay())
+alert(d.getMinutes())
